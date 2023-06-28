d4a21b6fb414d5dc454da2cdf73fcd03cd3bdbe3
Loghi Perinpanayagam <lper1582@sysco.com>
2023-06-28 14:37:54 +0530

    Time: 29 ms (93.19%) | Memory: 55.7 MB (65.81%) - LeetSync

--- path-with-maximum-probability/path-with-maximum-probability.java
+++ path-with-maximum-probability/path-with-maximum-probability.java
@@ -15,22 +15,22 @@ class Solution {
 
         // dijikstra
 
-        Queue<double[]> q = new PriorityQueue<>((a,b) -> Double.compare(b[1],a[1]));
-        q.offer(new double[]{start, 1.0});
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
         double[] dis = new double[n];
-        dis[start] = 1;
+        dis[start] = 1.0;
         
 
         while(!q.isEmpty()){
-            double[] idx = q.poll();
+            int idx = q.poll();
             
-            for(double[] pair : gp[(int)idx[0]]){
+            for(double[] pair : gp[(int)idx]){
                 int dest = (int)pair[0];
 
-                if(dis[(int)idx[0]] * pair[1] > dis[dest]){
+                if(dis[(int)idx] * pair[1] > dis[dest]){
 
-                    dis[dest] = dis[(int)idx[0]] * pair[1];
-                    q.offer(new double[]{dest, dis[dest]});
+                    dis[dest] = dis[(int)idx] * pair[1];
+                    q.offer(dest);
 
                 }
             }
