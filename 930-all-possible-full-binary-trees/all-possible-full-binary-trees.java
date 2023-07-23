4eb3a94746c1996a45059aa44b5235c3fc3240b0
Loghi Perinpanayagam <lper1582@sysco.com>
2023-07-23 19:46:19 +0530

    Time: 3 ms (46.87%) | Memory: 46.6 MB (18.47%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<TreeNode> allPossibleFBT(int N) {
+        // Recursive: build all possible FBT of leftSubTree and rightSubTree with number n
+        if(N <= 0 || N % 2 == 0) return new ArrayList<>();
+        
+        //1. if N = 3 , the number of nodes combination are as follows
+        //      left    root    right
+        //       1       1        1 
+        //--------------N = 3, res = 1----------
+        
+        //2. if N = 5 , the number of nodes combination are as follows
+        //      left    root    right
+        //       1       1        3 (recursion)
+        //       3       1        1 
+        //  --------------N = 5, res = 1 + 1 = 2----------
+        
+        //3. if N = 7 , the number of nodes combination are as follows
+        //      left    root    right
+        //       1       1        5 (recursion)
+        //       3       1        3 
+        //       5       1        1
+        //  --------------N = 7, res = 2 + 1 + 2 = 5----------
+        
+        //4. in order to make full binary tree, the node number must increase by 2
+        List<TreeNode> res = new ArrayList<>();
+        if(N == 1) {
+            res.add(new TreeNode(0));
+            return res;
+        }
+        for(int i = 1; i < N; i += 2) {
+            List<TreeNode> leftSubTrees = allPossibleFBT(i);
+            List<TreeNode> rightSubTrees = allPossibleFBT(N - i - 1);
+            for(TreeNode l : leftSubTrees) {
+                for(TreeNode r : rightSubTrees) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
